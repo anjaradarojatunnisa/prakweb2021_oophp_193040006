0f88d7e8f2e65f5adb80ce9ffea1256c33b4456b
AnjaraDNisa12 <193040006.anjara@mail.unpas.ac.id>
2021-09-25 08:50:12 -0700

    tugas 2 video 7

---
+++ constant.php
@@ -0,0 +1,35 @@
+<?php
+
+// define('Nama', 'anjara darojatun nisa');
+
+// echo Nama;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'sandhika';
+// }
+
+// echo Coba::NAMA;
+
+
+// echo __FILE__;
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+?>
